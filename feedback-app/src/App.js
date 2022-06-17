ad3e94a3f818d6178d7ea6784a28fc94d4ce0fa5
Brento27 <lombaardbrent@gmail.com>
2022-06-17 11:36:59 +0200

    Intro to JSX vid 9

--- feedback-app/src/App.js
+++ feedback-app/src/App.js
@@ -1,5 +1,9 @@
 function App() {
-  return <h1>Hello from the app component,this is very cool 1</h1>
+  return (
+    <div className="container">
+      <h1>My App</h1>
+    </div>
+  )
 }
 
 export default App
