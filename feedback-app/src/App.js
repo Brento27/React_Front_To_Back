506fbe5d6e0ae86e6a277d645040962cd52aafdd
Brento27 <lombaardbrent@gmail.com>
2022-06-21 13:51:02 +0200

    Creating Links (v5 & v6) vid 27

--- feedback-app/src/App.js
+++ feedback-app/src/App.js
@@ -5,6 +5,7 @@ import Header from "./components/Header"
 import FeedbackList from "./components/FeedbackList"
 import FeedbackStats from "./components/FeedbackStats"
 import FeedbackForm from "./components/FeedbackForm"
+import AboutIconLink from "./components/AboutIconLink"
 import AboutPage from "./pages/AboutPage"
 
 import FeedbackData from "./data/FeedbackData"
@@ -42,10 +43,10 @@ function App() {
               </>
             }
           ></Route>
-
           <Route path="/about" element={<AboutPage />} />
         </Routes>
       </div>
+      <AboutIconLink />
     </Router>
   )
 }
