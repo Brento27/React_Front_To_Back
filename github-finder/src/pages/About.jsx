3b14e6d0125d1e53755d972d74f265a3415132ad
Brent Lombaard <lombaardbrent@gmail.com>
2022-12-12 07:23:30 +0200

    Update About.jsx

--- github-finder/src/pages/About.jsx
+++ github-finder/src/pages/About.jsx
@@ -1,11 +1,12 @@
-import React from "react"
+import React from 'react';
 
 function About() {
   return (
     <>
-      <h1 className="text-6xl mb-4">Github Finder</h1>
-      <p className="mb-4 text-2xl font-light">
-        A React app to search GitHub profiles and see profile details. This
+      <h1 className='text-6xl mb-4'>Github Finder</h1>
+      <p className='mb-4 text-2xl font-light'>
+        A React app to search GitHub profiles and see profile details.
+        {/* This
         project is part of the
         <a href="https://www.udemy.com/course/modern-react-front-to-back/">
           {" "}
@@ -13,21 +14,21 @@ function About() {
         </a>{" "}
         Udemy course by
         <strong>
-          <a href="https://traversymedia.com"> Brad Traversy</a>
+          <a href="https://traversymedia.com">Brent Lombaard</a>
         </strong>
         .
       </p>
       <p className="text-lg text-gray-400">
-        Version <span className="text-white">1.0.0</span>
+        Version <span className="text-white">1.0.0</span> */}
       </p>
-      <p className="text-lg text-gray-400">
+      <p className='text-lg text-gray-400'>
         Layout By:
-        <a className="text-white" href="https://twitter.com/hassibmoddasser">
+        <a className='text-white' href='https://twitter.com/hassibmoddasser'>
           Hassib Moddasser
         </a>
       </p>
     </>
-  )
+  );
 }
 
-export default About
+export default About;
